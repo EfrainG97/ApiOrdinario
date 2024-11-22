c9b4f395051c970628e7e3aeecfc55e390f6b907
Efrain <112557@alumnouninter.mx>
2024-11-22 17:32:37 -0600

    Agregar archivos de proyecto.

---
+++ ApiOrdinario/Controllers/PersonajeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiOrdinario.Model;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace ApiOrdinario.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonajeController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public PersonajeController(HttpClient client)
+        {
+            _client = client;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Personaje>>> GetPersonajes()
+        {
+            var response = await _client.GetAsync("https://hp-api.onrender.com/api/characters");
+            var content = await response.Content.ReadAsStringAsync();
+            var personajes = JsonConvert.DeserializeObject<IEnumerable<Personaje>>(content);
+
+            return Ok(personajes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Personaje>> GetPersonajeID(int id)
+        {
+            var response = await _client.GetAsync($"https://hp-api.onrender.com/api/characters/{id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode, $"Error en la API externa: {response.ReasonPhrase}");
+            }
+            var content = await response.Content.ReadAsStringAsync();
+            var personaje = JsonConvert.DeserializeObject<Personaje>(content);
+            return Ok(personaje);
+        }
+
+        [HttpGet("casa/{houseName}")]
+        public async Task<ActionResult<IEnumerable<Personaje>>> GetPersonajesCasa(string houseName)
+        {
+            var response = await _client.GetAsync($"https://hp-api.onrender.com/api/characters/house/{houseName}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode, $"Error en la API externa: {response.ReasonPhrase}");
+            }
+            var content = await response.Content.ReadAsStringAsync();
+            var personajes = JsonConvert.DeserializeObject<IEnumerable<Personaje>>(content);
+            return Ok(personajes);
+        }
+
+    }
+}
---
+++ ApiOrdinario/Model/Personaje.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiOrdinario.Model
+{
+    public class Personaje
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string House { get; set; }
+        public string Gender { get; set; }
+        public string Patronus { get; set; }
+        public string Image { get; set; }
+    }
+}
